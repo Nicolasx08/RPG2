efdf5f61ce94ac244d41de03ff48c7a31bad2405
Nicolas Pageau <nic.pageau@hotmail.com>
2018-02-12 13:16:37 -0500

    Combat Fatale partie 2

--- src/sort/MortSubite.java
+++ src/sort/MortSubite.java
@@ -1,23 +1,23 @@
 package sort;
 
+import personnage.Magicien;
 import personnage.Personnage;
 
 /**
  * Created by PagNi1731620 on 2018-02-05.
  */
-public class MortSubite extends Sort {
+public class MortSubite extends Sort{
     int chance=0;
     public MortSubite(){
         setCout(10);
     }
 
-    public void lancerSort(Personnage personnage) {
-        chance=(int)(Math.random()*(10)+1);
-        if (chance==10){
+    public void lancerSort(Personnage personnage, Magicien magicien) {
+        chance = (int) (Math.random() * (10) + 1);
+        if (chance == 10) {
             personnage.setHp(0);
             System.out.println("Vous l'avez tuer d'un coup!!");
-        }
-        else {
+        } else {
             System.out.println("Votre attaque n'a pas atteint la cible.");
         }
     }
